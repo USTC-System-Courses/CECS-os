e21e1614ff6aaffac4b18c6a76f8288f3398aeaf
MaZirui <454434635@qq.com>
2023-09-14 18:09:28 +0800

    update os-app

--- os-app/libs/libos/src/crt0/crt0.c
+++ os-app/libs/libos/src/crt0/crt0.c
@@ -5,10 +5,10 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  int argc = args[0];
-  char **argv = (char**)args + 1;
-  char **envp = (char**)argv + argc + 1;
-  environ = envp;
-  exit(main(argc, argv, envp));
+  // int argc = args[0];
+  // char **argv = (char**)args + 1;
+  // char **envp = (char**)argv + argc + 1;
+  // environ = NULL;
+  exit(main(0, NULL, NULL));
   assert(0);
 }
